5cd52d735785afd22a62b75cb182849d8c2f3d98
samy <ryio69007@gmail.com>
2019-06-10 03:45:11 +0200

    initial commit create Main.java  Flight.java

---
+++ src/com/samy/calcengine/Flight.java
@@ -0,0 +1,14 @@
+package com.samy.calcengine;
+
+public class Flight {
+    private int passengers;
+    private int seats;
+
+    public Flight() {
+        seats = 150;
+        passengers = 0;
+    }
+    public void add1Passenger(){
+
+    }
+}
---
+++ src/com/samy/calcengine/Main.java
@@ -0,0 +1,8 @@
+package com.samy.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
