e2f76c206c722a1d4cba9e102a9ca63d4565b457
samy <samy.t.khelifa@gmail.com>
2019-06-19 20:59:59 +0200

    modify :  CrewMember.java

--- src/com/samy/flight/CrewMember.java
+++ src/com/samy/flight/CrewMember.java
@@ -1,4 +1,13 @@
 package com.samy.flight;
 
 public class CrewMember {
+    private FlightCrewJob job;
+
+    public CrewMember(FlightCrewJob job) {
+        this.job = job;
+    }
+
+    public void setJob(FlightCrewJob job) {
+        this.job = job;
+    }
 }
--- src/com/samy/flight/Main.java
+++ src/com/samy/flight/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Passenger july = new Passenger(34);
         System.out.println("july has " + july.getFreeBags() + " free bags");
-        Flight f744= new Flight();
+        Flight f744 = new Flight();
         f744.setSeats(551);
-        System.out.println("Flight 744 has "+ f744.getSeats()+ " seats");
-
+        System.out.println("Flight 744 has " + f744.getSeats() + " seats");
+        CrewMember carol = new CrewMember(FlightCrewJob.Copilot);
+        System.out.println("carol is a " + FlightCrewJob.Copilot);
 
 
     }
